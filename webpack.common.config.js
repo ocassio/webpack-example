426e0c669a9ddc74d9742a163496c4c74a551c04
Alexander Ionov <orlicus@gmail.com>
2020-12-07 21:25:18 +0400

    Explicit public path

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        publicPath: '',
+        publicPath: '/',
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
